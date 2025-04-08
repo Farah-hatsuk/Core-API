834a37cdce9205e461fefc8c6f210a48da2c4620
Farah-hatsuk <farah.hatsuk@gmail.com>
2025-04-08 17:26:47 +0300

    task1

---
+++ coreAPI/coreAPI.Server/Controllers/CategoryController.cs
@@ -0,0 +1,59 @@
+﻿using coreAPI.Server.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreAPI.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+
+        public CategoryController(MyDbContext db) 
+        {
+            _db = db;
+        }
+
+        [HttpGet("getCategory")]
+        public IActionResult getAllCategory()
+        {
+            var categoty = _db.Categories.ToList();
+            return Ok(categoty);
+        }
+
+        [HttpGet("getCategoryById")]
+        public IActionResult getCategoryById(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+           if (category != null)
+            {
+                return Ok(category); //200
+            }
+            else
+            {
+                return NotFound(); // 404
+            }
+        }
+
+        [HttpGet("getCategoryByName")]
+        public IActionResult getCategoryByName(string name) 
+        { 
+            var category = _db.Categories.FirstOrDefault(x => x.Name == name);
+            if (category != null)
+            {
+                return Ok(category);
+            }
+            else { 
+            return NotFound();
+            }
+        }
+
+        [HttpGet("getFirstCategory")]
+        public IActionResult getFirstCategory()
+        {
+            var FirstCategory = _db.Categories.First();
+            return Ok(FirstCategory);
+        }
+    }
+}
---
+++ coreAPI/coreAPI.Server/Models/MyDbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace coreAPI.Server.Models;
+
+public partial class MyDbContext : DbContext
+{
+    public MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-F194SCB;Database=NetCoreAPI;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Category__3214EC078E95C6C5");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("product");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .HasColumnName("name");
+            entity.Property(e => e.Price).HasColumnName("price");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
